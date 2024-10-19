c91e74dc8f71e2977f8e432cc26411780c483d7a
KrityaShah <118177316+KrityaShah@users.noreply.github.com>
2024-10-19 22:31:10 +0545

    Created home
    imported to app

---
+++ client/src/components/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./Home.css"
+
+
+const Home = () => {
+  return (
+    <div className='Home-Container'>
+        <h1>Do you want to write some code?</h1>
+        <div className="home-bottom">
+        <button>Code now</button>
+        </div>
+    </div>
+  )
+}
+
+export default Home
