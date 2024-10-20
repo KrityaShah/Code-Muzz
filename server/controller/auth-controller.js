b0b30888d3e8036fed4879527b5aefddebdfab36
KrityaShah <118177316+KrityaShah@users.noreply.github.com>
2024-10-20 11:44:47 +0545

    Db connection

--- server/controller/auth-controller.js
+++ server/controller/auth-controller.js
@@ -1,12 +1,51 @@
+const User = require('../model/user-mode');
+const bcrypt = require('bcryptjs')
+
 // Home page:
-
-
-
 const home = async (req, res) => {
-    return res
-    .status(200)
-    .send("Welcome to home using controller");
+    try {
+        return res
+        .status(200)
+        .send("Welcome to home using controller");
+    } catch (error) {
+        console.error(error);
+        
+    }
+   
+}
+
+// Registration:
+
+const register = async (req, res) =>{
+    try {
+
+        const {username, password, email, phone } = req.body;
+
+        const userExist = await User.findOne({email})
+
+        if(userExist){
+            return res
+            .status(400)
+            .json({Message: "Email already Exist!"});
+        }
+
+        const saltRound = 10;
+        const hash_password = await bcrypt.hash(password, saltRound);
+
+        const userCreated = await User.create({username, password : hash_password, email, phone});
+
+        res
+        .status(201)
+        .json({Message: "Registration Sucessfull!"})
+    } catch (error) {
+        console.error(error);
+        
+    }
 }
 
 
-module.exports = {home};
+// Login
+
+
+
+module.exports = {home, register};
--- server/index.js
+++ server/index.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = 5000;
 const router = require("./router/auth-router");
+const connectDb = require("./utils/db");
 
+
+app.use(express.json());
 app.use("/api/auth/", router);
 
-app.listen(PORT, ()=>{
-    console.log(`Server is running in ${PORT}`);
-    
+connectDb().then(() => {
+    app.listen(PORT, ()=>{
+        console.log(`Server is running in ${PORT}`);
+        
+    })
 })
+
