f4ed7adf527824d33b4f76aefc02a9d8613924c3
KrityaShah <118177316+KrityaShah@users.noreply.github.com>
2024-10-20 11:46:18 +0545

    Db connection

--- server/index.js
+++ server/index.js
@@ -1,17 +1,11 @@
 const express = require('express');
 const app = express();
-const PORT = 5000;
+const PORT = 3000;
 const router = require("./router/auth-router");
-const connectDb = require("./utils/db");
 
-
-app.use(express.json());
 app.use("/api/auth/", router);
 
-connectDb().then(() => {
-    app.listen(PORT, ()=>{
-        console.log(`Server is running in ${PORT}`);
-        
-    })
+app.listen(PORT, ()=>{
+    console.log(`Server is running in ${PORT}`);
+    
 })
-
