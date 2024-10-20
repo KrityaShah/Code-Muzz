fede4c36a2c0489ab72273e36d8d6f111457654e
KrityaShah <118177316+KrityaShah@users.noreply.github.com>
2024-10-20 11:43:12 +0545

    browser router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,62 +1,27 @@
 import React from "react"
-import { useState, useEffect } from "react"
-import Editor from "./components/Editor"
 import Navbar from "./components/Navbar"
 import Home from "./components/Home"
+import Code from "./components/Code";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 function App() {
-// const [html, setHtml] = useState('')
-// const [css, setCss] = useState('')
-// const [js, setJs] = useState('')
-// const [srcDoc, setSrcDoc] = useState('')
-
-//   useEffect(() => {
-//    const timeout = setTimeout(() => {
-//       setSrcDoc(`
-//   <html>
-//   <body>${html}</body>
-//   <style>${css}</style>
-//   <script>${js}</script>
-//   </html>
-//   `)
-//     }, 250);
-
-//     return () => clearTimeout(timeout)
-//   }, [html, css, js])
-  
-
-  
 
   return (
     <>  
-
-    <Navbar/>
-    <Home/>
-      {/* <div className="pane top-pane">
-        <Editor language= "xml"
-         displayName = "HTML" 
-         value = {html} 
-         onChange={setHtml}/>
-        
-        <Editor language= "css"
-         displayName = "CSS" 
-         value = {css} 
-         onChange={setCss}/>
-
-<Editor language= "javascript"
-         displayName = "JS" 
-         value = {js} 
-         onChange={setJs}/>
-      </div>
-      <div className="pane">
-      <iframe
-      srcDoc= {srcDoc}
-      title="output"
-      sandbox="allow-scripts"
-      width= "100%"
-      height="100%"
-      />
-      </div> */}
+  <Router>
+    <Routes>
+      <Route path="/" element={<><Navbar/><Home/></>}/>
+      <Route path="/code" element={<><Navbar/><Code/></>}/>
+      {/* <Route path="/editor" element={<><Editor/></>}/> */}
+    </Routes>
+  </Router>
+    
+  
+ 
     </>
   )
 }
