643432a0c87355f5826d962c70563644816e67fa
KrityaShah <118177316+KrityaShah@users.noreply.github.com>
2024-10-20 11:42:47 +0545

    Routed code

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import "./Home.css"
-
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
     <div className='Home-Container'>
         <h1>Do you want to write some code?</h1>
         <div className="home-bottom">
-        <button>Code now</button>
+     <Link to="/code" className="nav-link"><button>Code now</button></Link> 
         </div>
     </div>
   )
