73f5696411b536e908bfa0c3e0f4a9a0e2add84b
KrityaShah <118177316+KrityaShah@users.noreply.github.com>
2024-10-20 11:41:28 +0545

    Updated style of navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import "./Navbar.css"
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
    <nav>
     <h1>Code-Editor</h1>
     <ul>
-        <li>Home</li>
-        <li>Profile</li>
-        <li>Code</li>
-        <li>Logout</li>
+       <Link to="/" className="nav-link"><li className="nav-item">Home</li></Link> 
+       <Link to="/profile" className="nav-link"><li className="nav-item">Profile</li></Link> 
+       <Link to="/code" className="nav-link"><li className="nav-item">Code</li></Link> 
+       <Link to="/" className="nav-link"><li className="nav-item">Logout</li></Link> 
     </ul>
    </nav>
   )
