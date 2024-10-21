bbc294edc7e1e208658ea9e93109e3c702cbb463
KrityaShah <118177316+KrityaShah@users.noreply.github.com>
2024-10-21 12:52:24 +0545

    Updated Home

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,15 +1,22 @@
 import React from 'react'
 import "./Home.css"
 import { Link } from 'react-router-dom'
+import Signup from './Signup'
+import Login from './Login'
 
 const Home = () => {
   return (
     <div className='Home-Container'>
         <h1>Do you want to write some code?</h1>
         <div className="home-bottom">
-     <Link to="/code" className="nav-link"><button>Code now</button></Link> 
+     <Link to="/signup" className="nav-link"><button>Code now</button></Link> 
         </div>
     </div>
+    
+    // <Signup/>
+    // <Login/>
+
+  
   )
 }
 
